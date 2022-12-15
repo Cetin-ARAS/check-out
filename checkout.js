4dd42c04a5e8be7acd6a20a524f88205f5a74726
ÇETİN <cetinnnarasss@gmail.com>
2022-12-15 15:48:08 +0300

    cont.

--- checkout.js
+++ checkout.js
@@ -14,16 +14,52 @@ window.addEventListener("load", ()=>{
 let productsDiv = document.querySelector(".products");
 productsDiv.addEventListener("click", (event)=>{
     if(event.target.className =="minus"){
-        event.target.nextElementSibling.innerText--; //* sonraki kardeşini innertextini 1 azalt demek
+        if(event.target.nextElementSibling.innerText > 1){
+            event.target.nextElementSibling.innerText--; //* sonraki kardeşini innertextini 1 azalt demek
+            //parameter == selected productInfoDiv
+            calculateProductAndCartTotal(element.target.parentElement.parentElement);
+
+        }else{
+            if(confirm("Product will be deleted")){
+               event.target.parentElement.parentElement.parentElement.remove();
+               calculateCartTotal();
+               
+            }
+
+        }
+        
+        
         //*console.log("minus button clicked");
     }else if(event.target.classList.contains("plus")){
         event.target.previousElementSibling.innerText++;
+        //parameter == selected productInfoDiv
+        calculateProductAndCartTotal(element.target.parentElement.parentElement);
         //*console.log("plus button clicked"
     }else if(event.target.classList.contains("remove-product")){
         event.target.parentElement.parentElement.parentElement.remove();
+        calculateCartTotal();
+
         //*console.log("remove button clicked")
     }
     else{
         console.log("other element clicked")
     }
 });
+
+//* calculate cart and product totals
+const calculateProductAndCartTotal = (productInfoDiv) =>{
+ // product calculation  
+ let quentity =
+ let price =
+
+
+
+// cart calculation
+const calculateCartTotal();
+}
+
+//* calculate cart totals
+const calculateCartTotal() => {
+
+}
+

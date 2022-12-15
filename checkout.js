0face78628c5121be6a6677a2d8700f14c0da14f
ÇETİN <cetinnnarasss@gmail.com>
2022-12-16 02:34:11 +0300

    finish

--- checkout.js
+++ checkout.js
@@ -1,10 +1,10 @@
-const taxRate = 0.18;
-const shippingPrice = 15.0;
+// const taxRate = 0.18;
+// const shippingPrice = 15.0;
 
 window.addEventListener("load", ()=>{
     //!set item to LocalStorage
-   localStorage.setItem("taxRate", taxRate);
-   localStorage.setItem("shippigPrice", shippingPrice);
+//    localStorage.setItem("taxRate", taxRate);
+//    localStorage.setItem("shippigPrice", shippingPrice);
     //!set item to SessionStorage
 //    sessionStorage.setItem("taxRate", taxRate);
 //    sessionStorage.setItem("shippigPrice", shippingPrice);
@@ -62,5 +62,22 @@ calculateCartTotal();
 
 //* calculate cart totals
 const calculateCartTotal = () =>{
+    // nodeList Div
+    let  productsTotalPriceDivs = document.querySelectorAll(".product-line-price")  // documentın amacı tüm productlerı almak
+    let subTotal = 0;
+    productsTotalPriceDivs.forEach((productsTotalPriceDiv)=>{
+        subTotal += parseFloat(productsTotalPriceDiv.innerText)
+    });
+    
+    let taxPrice = subTotal * localStorage.getItem("taxRate")
+    let shippingPrice = (subTotal > 0 ? parseFloat(localStorage.getItem("shippingPrice")) : 0);
+    let cartTotal = subTotal + taxPrice + shippingPrice;
 
+    document.querySelector("#cart-subtotal p:nth-child(2)").innerText = subTotal.toFixed(2)
+
+    document.querySelector("#cart-tax p:nth-child(2)").innerText = taxPrice.toFixed(2)
+   
+    document.querySelector("#cart-shipping p:nth-child(2)").innerText = shippingPrice.toFixed(2)
+
+    document.querySelector("#cart-total").lastElementChild.innerText = cartTotal.toFixed(2)
 }

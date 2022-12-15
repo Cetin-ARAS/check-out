fa544e7450d83c677460ef6beb0ca851ac327ac3
ÇETİN <cetinnnarasss@gmail.com>
2022-12-15 04:11:38 +0300

    storages

--- checkout.js
+++ checkout.js
@@ -1,2 +1,11 @@
 const textRate = 0.18;
 const shippingPrice = 15.0;
+
+window.addEventListener("load", ()=>{
+    //!set item to LocalStorage
+   localStorage.setItem("texRate", texRate);
+   localStorage.setItem("shippigPrice", shippingPrice);
+    //!set item to SessionStorage
+   sessionStorage.setItem("texRate", texRate);
+   sessionStorage.setItem("shippigPrice", shippingPrice);
+});
